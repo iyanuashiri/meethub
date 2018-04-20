f753f93192712e8badbb8ad660f98c5471566fed
Ashiri <ajaoiyanu@gmail.com>
2018-04-20 16:43:38 +0100

    Add events admin

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
+from .models import Category, Comment, Event
+
 # Register your models here.
+
+
+class CategoryAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'description')
+    fields = ('name', 'description')
+
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class EventAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'venue', 'date', 'time')
+    fields = ('category', 'name', 'creator', 'details', 'venue', ('date', 'time'))
+    search_fields = ('name', 'details')
+
+admin.site.register(Event, EventAdmin)
+
+
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('post', 'created_date', 'created_time')
+    fields = ('event', 'post', 'created_by')
+    search_fields = ('post',)
+
+
+admin.site.register(Comment, CommentAdmin)
