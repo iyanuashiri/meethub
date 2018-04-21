91ec359cd842000fd180274ce211cb8d82df183e
Ashiri <ajaoiyanu@gmail.com>
2018-04-21 21:08:18 +0100

    Fixed home url

--- events/urls.py
+++ events/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'events'
 urlpatterns = [
-    path('events/', views.EventList.as_view(), name='event-list'),
+    path('', views.EventList.as_view(), name='event-list'),
     path('events/<int:pk>/', views.event_detail, name='event-detail'),
     path('events/new/', views.EventCreate.as_view(), name='event-create' ),
     path('events/<int:pk>/delete', views.EventDelete.as_view(), name='event-delete'),
