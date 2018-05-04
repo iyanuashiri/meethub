3ffe4dd77321e0843e0fc582b88979c9541c040e
Ashiri <ajaoiyanu@gmail.com>
2018-05-04 11:01:56 +0100

    Add actions admin

--- actions/admin.py
+++ actions/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Action
+
 # Register your models here.
+
+class ActionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'verb', 'target', 'created')
+    list_filter = ('created',)
+    search_fields = ('verb',)
+
+
+admin.site.register(Action, ActionAdmin)
