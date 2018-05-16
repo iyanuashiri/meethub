3faec83e20b5b4b58c2743ca607fbda4bdc83a6b
Ashiri <ajaoiyanu@gmail.com>
2018-05-16 19:06:22 +0100

    Add events test

---
+++ events/tests/test_forms.py
@@ -0,0 +1,103 @@
+from django.test import TestCase
+
+from events.forms import EventForm, CommentForm
+from events.models import Event, Comment, Category, User
+from userprofile.models import Profile
+
+
+class EventFormTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        category = Category.objects.create(name='Technology', description='This is the future')
+        user = User.objects.create(username='iyanu', password=12345, email='iyanu@gmail.com')
+        Event.objects.create(name='Party Outside', details='This party is gonna be banging again', venue='Mapo Hall',
+                             date='2018-05-18', time='12:25:00', category=category, creator=user)
+
+    def test_event_form_valid(self):
+        form = EventForm(
+            data={'name': 'Party Outside', 'details': 'This party is gonna be banging again', 'venue': 'Mapo Hall',
+                  'date': '2018-05-18', 'time': '12:25:00', 'category': 'category',})
+        self.assertTrue(form.is_valid())
+
+    def test_event_form_invalid(self):
+        form = EventForm(
+            data={'name': '', 'details': '', 'venue': 'Mapo Hall',
+                  'date': '2018-05-18', 'time': '12:25:00', 'category': 'category', 'creator': 'user'})
+        self.assertFalse(form.is_valid())
+
+    def test_event_form_date_field_label(self):
+        form = EventForm()
+        self.assertTrue(form.fields['date'].label == None or form.fields['date'].label == 'Date')
+
+    def test_event_form_date_field_help_text(self):
+        form = EventForm()
+        self.assertEqual(form.fields['date'].help_text, 'Please use the following format: <em>YYYY-MM-DD</em>.')
+
+    def test_event_form_time_field_label(self):
+        form = EventForm()
+        self.assertTrue(form.fields['time'].label == None or form.fields['time'].label == 'Time')
+
+    def test_event_form_time_field_help_text(self):
+        form = EventForm()
+        self.assertEqual(form.fields['time'].help_text, 'Please use the following format: <em>HH:MM:SS<em>')
+
+    def test_event_form_name_field_label(self):
+        form = EventForm()
+        self.assertTrue(form.fields['name'].label == None or form.fields['name'].label == 'Name')
+
+    def test_event_form_details_field_label(self):
+        form = EventForm()
+        self.assertTrue(form.fields['details'].label == None or form.fields['details'].label == 'Details')
+
+    def test_event_form_venue_field_label(self):
+        form = EventForm()
+        self.assertTrue(form.fields['venue'].label == None or form.fields['venue'].label == 'Venue')
+
+    def test_event_form_category_field_label(self):
+        form = EventForm()
+        self.assertTrue(form.fields['category'].label == None or form.fields['category'].label == 'Category')
+
+
+class CommentFormTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        category = Category.objects.create(name='Technology', description='This is the future')
+        user = User.objects.create(username='iyanu', password=12345, email='iyanu@gmail.com')
+        event = Event.objects.create(name='Party Out', details='This party is gonna be banging', venue='Ibadan',
+                                     date='2018-06-18', time='12:00:00', category=category, creator=user)
+        Comment.objects.create(comment='Hey yo', event=event, created_by=user)
+        Profile.objects.create(date_of_birth='2018-12-12', user=user)
+
+    def test_comment_form_valid(self):
+        form = CommentForm(
+            data={'comment': 'Hey yo'})
+        self.assertTrue(form.is_valid())
+
+    def test_comment_form_invalid(self):
+        form = CommentForm(
+            data={'comment': ''})
+        self.assertFalse(form.is_valid())
+
+    def test_event_form_comment_field_label(self):
+        form = EventForm()
+        self.assertTrue(form.fields['comment'].label == None or form.fields['comment'].label == 'Comment')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ events/tests/test_models.py
@@ -0,0 +1,103 @@
+from django.test import TestCase
+
+from events.models import Event, Category, Comment, User
+from userprofile.models import Profile
+
+
+class EventModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        category = Category.objects.create(name='Technology', description='This is the future')
+        user = User.objects.create(username='iyanu', password=12345, email='iyanu@gmail.com')
+        Event.objects.create(name='Party Outside', details='This party is gonna be banging again', venue='Mapo Hall',
+                             date='2018-05-18', time='12:25:00', category=category, creator=user)
+
+    def test_name_label(self):
+        event = Event.objects.get(name='Party Outside')
+        field_label = event._meta.get_field('name').verbose_name
+        self.assertEquals(field_label, 'name')
+
+    def test_details_label(self):
+        event = Event.objects.get(name='Party Outside')
+        field_label = event._meta.get_field('details').verbose_name
+        self.assertEquals(field_label, 'Details')
+
+    def test_venue_label(self):
+        event = Event.objects.get(name='Party Outside')
+        field_label = event._meta.get_field('venue').verbose_name
+        self.assertEquals(field_label, 'venue')
+
+    def test_date_label(self):
+        event = Event.objects.get(name='Party Outside')
+        field_label = event._meta.get_field('date').verbose_name
+        self.assertEquals(field_label, 'date')
+
+    def test_time_label(self):
+        event = Event.objects.get(name='Party Outside')
+        field_label = event._meta.get_field('time').verbose_name
+        self.assertEquals(field_label, 'time')
+
+    def test_name_max_length(self):
+        event = Event.objects.get(name='Party Outside')
+        max_length = event._meta.get_field('name').max_length
+        self.assertEquals(max_length, 50)
+
+    def test_venue_max_length(self):
+        event = Event.objects.get(name='Party Outside')
+        max_length = event._meta.get_field('venue').max_length
+        self.assertEquals(max_length, 200)
+
+    def test_event_object_name(self):
+        event = Event.objects.get(name='Party Outside')
+        expected_object_name = '{}'.format(event.name)
+        self.assertEquals(expected_object_name, str(event))
+
+    def test_get_absolute_url(self):
+        event = Event.objects.get(name='Party Outside')
+        self.assertEquals(event.get_absolute_url(), '/events/4/')
+
+    def test_get_number_of_attendees(self):
+        event = Event.objects.get(name='Party Outside')
+        self.assertEquals(event.get_number_of_attendees(), 0)
+
+    def test_get_comments_number(self):
+        event = Event.objects.get(name='Party Outside')
+        self.assertEquals(event.get_comments_number(), 0)
+
+
+class CategoryModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Category.objects.create(name='Technology', description='This is the future')
+
+    def test_category_object_name(self):
+        category = Category.objects.get(name='Technology')
+        expected_object_name = '{}'.format(category.name)
+        self.assertEquals(expected_object_name, str(category))
+
+
+class CommentModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        category = Category.objects.create(name='Technology', description='This is the future')
+        user = User.objects.create(username='iyanu', password=12345, email='iyanu@gmail.com')
+        event = Event.objects.create(name='Party Out', details='This party is gonna be banging', venue='Ibadan',
+                                     date='2018-06-18', time='12:00:00', category=category, creator=user)
+        Comment.objects.create(comment='Hey yo', event=event, created_by=user)
+        Profile.objects.create(date_of_birth='2018-12-12', user=user)
+
+    def test_comment_object_name(self):
+        comment = Comment.objects.get(comment='Hey yo')
+        expected_object_name = '{}'.format(comment)
+        self.assertEquals(expected_object_name, str(comment))
+
+    def test_get_absolute_url(self):
+        comment = Comment.objects.get(comment='Hey yo')
+        self.assertEquals(comment.get_absolute_url(), '/events/2/')
+
+    def test_get_comment_creator_photo(self):
+        comment = Comment.objects.get(comment='Hey yo')
+        self.assertEquals(comment.get_comment_creator_photo(), None)
