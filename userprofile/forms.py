296a1494ab6f2828b61f0b8e4ad80308306fc23a
Ashiri <ajaoiyanu@gmail.com>
2018-06-19 17:29:38 +0100

    Fix userprofile

--- userprofile/forms.py
+++ userprofile/forms.py
@@ -1,11 +1,11 @@
 from django import forms
 from django.contrib.auth.models import User
 
-
 from .models import Profile
 
 
 class UserForm(forms.ModelForm):
+
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'email')
