2df049a57301663c2b6d40d74f83cc7fabf4542e
Iyanuoluwa Ajao <ajaoiyanu@gmail.com>
2025-07-08 15:54:37 +0100

    refactor and fix

--- apiv1/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class Apiv1Config(AppConfig):
-    name = 'apiv1'
--- apiv1/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import permissions, reverse, generics
-
-# Create your views here.
-
